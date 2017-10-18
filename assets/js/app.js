e470797a89e88e9984ff29c18af887a8ce132c7b
AndrewD14 <Leingod2005@gmail.com>
2017-10-18 10:51:04 -0500

    Changed the api calls to use the random api and then id endpoint to try not to always have the same gifs show up. Added in a message saying images are being retrieved.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,50 @@
 //global variables
 var topic = ["Super Sayian", "Final Fantasy", "Zelda", "Link", "Cowboy Beebop", "Trigun", "Dragon Ball Z", "Goku", "Vegeta", "Dragon Ball Super", "Cloud Strife", "Tifa Lockhart",
 			 "Horizon Zero Dawn", "Sephiroth"];
-var imageObjects = {};
+var imageObjects = [];
+var limits = 10;
+var errorLimit = 5;
+var errorCount = 0;
+var currentSearch = "";
+
+var apikey = "bbPNFKOvjhX2X9elWklBGJiZqggvbajP";
 
 //function to grab data from the api
-function getGif(search){
+function getGif(){
+	//only runs if there is a search term
+	if(currentSearch != null && currentSearch != ""){
+		//various api parameters
+		var queryURL = "https://api.giphy.com/v1/gifs/random";
+		
+		var maxRatingAllowed = "pg-13";
+		
+		var inputs = {};
+
+		//adds the elements to the url if the variable have data
+		if(apikey != null && apikey != "")
+			inputs.api_key = apikey;
+
+		inputs.tag = currentSearch;
+
+		if(maxRatingAllowed != null && maxRatingAllowed != "")
+			inputs.rating = maxRatingAllowed;
+
+		//calls the api and executes functions based on success or failure
+		$.ajax({
+			url: queryURL,
+			method: "GET",
+			data: inputs
+		})
+		.done(generatePictures)
+		.fail(apiError)
+		;
+	}
+}
+
+//gets the img info with the id from the random api call
+function getImgInfo(id){
 	//various api parameters
-	var queryURL = "https://api.giphy.com/v1/gifs/search";
-	var apikey = "bbPNFKOvjhX2X9elWklBGJiZqggvbajP";
-	var limits = 10;
-	var maxRatingAllowed = "pg-13";
+	var queryURL = "https://api.giphy.com/v1/gifs";
 	
 	var inputs = {};
 
@@ -17,14 +52,8 @@ function getGif(search){
 	if(apikey != null && apikey != "")
 		inputs.api_key = apikey;
 
-	if(limits != null && limits != "")
-		inputs.limit = limits;
-
-	if(search != null && search != "")
-		inputs.q = search;
-
-	if(maxRatingAllowed != null && maxRatingAllowed != "")
-		inputs.rating = maxRatingAllowed;
+	if(id != null && id != "")
+		inputs.ids = id;
 
 
 	//calls the api and executes functions based on success or failure
@@ -33,16 +62,43 @@ function getGif(search){
 		method: "GET",
 		data: inputs
 	})
-	.done(generatePictures)
+	.done(parseImgInfo)
 	.fail(apiError)
 	;
 }
 
-//processes the response from the api
+//processes the response from the api for random
 function generatePictures(response){
-	//stores the images
-	imageObjects = response.data;
+	//gets more info on the img
+	getImgInfo(response.data.id);
+}
 
+//process the img info
+function parseImgInfo(response){
+	var found = false;
+
+	//checks to make sure the image was not already pulled
+	for(i in imageObjects){
+		if(imageObjects[i].id === response.data[0].id)
+			found = true;
+	}
+	
+	if(!found)
+		//stores the images
+		imageObjects.push(response.data[0]);
+
+	//resets the error count since there was a success
+	errorCount = 0;
+
+	if(imageObjects.length < limits)
+		getGif();
+	else
+		displayImages();
+
+}
+
+//displays the pictures
+function displayImages(){
 	//clear the current gifs
 	$("#giphy").empty();
 
@@ -77,6 +133,13 @@ function apiError(response){
 	var responseText = $.parseJSON(response.responseText);
 	$("#error").html("There was an error calling the Giphy API.<br>Status: "+responseText.meta.status+"<br>"+responseText.meta.msg);
 	$("#error").css("display", "block");
+
+	//increments the error count
+	errorCount++;
+
+	//calls the api again
+	if(errorCount < errorLimit)
+		getGif();
 }
 
 //generates the buttons and populates it on the page
@@ -105,7 +168,20 @@ function searchForGif(){
 	$("#error").css("display", "none");
 	$("#error").html("");
 
-	getGif($(this).attr("search"));
+	//sets the search term
+	currentSearch = $(this).attr("search");
+
+	//clears the current image
+	imageObjects = [];
+
+	//clear the current gifs
+	$("#giphy").empty();
+
+	//displays a message for getting gifs
+	$("#giphy").html("Retrieving images... <img src='assets/images/loading.gif' height='30px' width='30px' />");
+
+	//calls the random Giphy API
+	getGif();
 }
 
 //onclick function to swap between animated and still
